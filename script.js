ebd8c67e5c1b2adbb0b72e1cc4c6e4f1b1c8ab64
Allan Gaia Pio <allan@pakman.com.br>
2021-09-28 15:51:56 -0300

    Redirecionando para nova página do Pontes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function setUp() {
   var btn = document.getElementById("btn-1");
   var section2 = document.getElementById("section-2");
-  
-  window.loaction.replace = "https://pontesdaterra.org/";
 
-  btn.addEventListener('click', function(e) {
+  btn.addEventListener('click', function (e) {
     e.preventDefault();
     var id = btn.getAttribute('href');
     var targetOffset = section2.offsetTop;
@@ -16,17 +14,17 @@ function setUp() {
   });
 }
 
-// function redirect() {
-//   window.loaction.replace = "https://pontesdaterra.org/";
-// }
+function redirect() {
+  window.location.href = "https://pontesdaterra.org/";
+}
 
-// redirect();
+redirect();
 
 // $('.nav a[href^="#"]').on('click', function(e) {
 //   e.preventDefault();
 //   var id = $(this).attr('href'),
 //       targetOffset = $(id).offset().top;
-      
+
 //   $('html, body').animate({ 
 //     scrollTop: targetOffset - 100
 //   }, 500);
